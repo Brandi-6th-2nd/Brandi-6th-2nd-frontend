acc4cf64d474df895288fe7877f311b725545ca2
Juhee Jang <juheejang.dev@gmail.com>
2020-10-21 12:58:34 +0900

    [12기 장주희] Add: 로그인 페이지 레이아웃 완성

---
+++ src/Components/LoginFooter/LoginFooter.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function LoginFooter() {
+  return (
+    <Fragment>
+      <CompanyInfo>
+        회사명 : (주)브랜디 | 주소 : (06223) 서울특별시 강남구 테헤란로 32길 26
+        청송빌딩 | 사업자등록번호 : 220-88-93187 | 통신판매업신고 :
+        2016-서울강남-00359호 <br /> 이메일 : help@brandi.co.kr | 2018 © brandi
+        inc.
+      </CompanyInfo>
+      <Service>
+        <TermsOfService>이용약관</TermsOfService>
+        <PrivacyPolicy>개인정보처리방침</PrivacyPolicy>
+      </Service>
+    </Fragment>
+  );
+}
+
+export default LoginFooter;
+
+const Fragment = styled.div`
+  width: 100%;
+  height: 107.6px;
+  background-color: black;
+  padding: 20px 0px;
+`;
+
+const CompanyInfo = styled.div`
+  width: 800px;
+  margin: 0 auto;
+  color: #999ba2;
+  font-size: 11px;
+  line-height: 20px;
+`;
+
+const Service = styled.div`
+  width: 800px;
+  margin: 8px auto 0;
+`;
+
+const TermsOfService = styled.a`
+  display: inline-block;
+  font-size: 11px;
+  color: white;
+  cursor: pointer;
+  :hover {
+    text-decoration: underline;
+  }
+`;
+
+const PrivacyPolicy = styled(TermsOfService.withComponent('a'))`
+  :before {
+    content: '|';
+    color: #999ba2;
+  }
+`;
--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,7 +1,123 @@
-import React from "react";
+import React from 'react';
+import styled from 'styled-components';
+import LoginFooter from '../../Components/LoginFooter/LoginFooter';
 
 function Login() {
-  return <div>Hello</div>;
+  return (
+    <Fragment>
+      <Content>
+        <Logo
+          src="public/Images/Login/admin_logo.png"
+          alt="브랜디 어드민 로고"
+        />
+        <LoginArea>
+          <AdminText>브랜디 어드민 로그인</AdminText>
+          <InputBox>
+            <UserName id="username" name="username" placeholder="셀러 아이디" />
+            <UserPassword
+              id="userpassword"
+              name="userpassword"
+              placeholder="셀러 비밀번호"
+            />
+          </InputBox>
+          <LoginButton>로그인</LoginButton>
+          <JoinArea>
+            <Text>아직 셀러가 아니신가요?</Text>
+            <GoToSignUp>회원가입하기</GoToSignUp>
+          </JoinArea>
+        </LoginArea>
+      </Content>
+      <LoginFooter />
+    </Fragment>
+  );
 }
 
 export default Login;
+
+const Fragment = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: #fafafa;
+`;
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 700px;
+  height: 100vh;
+  margin: auto;
+  padding: 65px 0px 50px;
+`;
+
+const Logo = styled.img`
+  width: 130px;
+  height: 45px;
+  margin-top: 10px;
+  margin-bottom: 40px;
+`;
+
+const LoginArea = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 380px;
+  height: 400px;
+  padding: 64px 30px 64px 30px;
+  background-color: #fff;
+  border-radius: 20px;
+  box-shadow: 0 4px 31px 0 rgba(0, 0, 0, 0.1);
+`;
+
+const AdminText = styled.p`
+  margin-bottom: 25px;
+  font-size: 24px;
+  font-weight: bolder;
+`;
+
+const InputBox = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  width: auto;
+  height: 110px;
+`;
+
+const UserName = styled.input`
+  width: 320px;
+  height: 45.6px;
+  padding: 13px 16px;
+  font-size: 12px;
+  border: 1px solid #e5e5e5;
+  border-radius: 7px;
+`;
+
+const UserPassword = styled(UserName.withComponent('input'))``;
+
+const LoginButton = styled.button`
+  width: 320px;
+  height: 44.8px;
+  margin-top: 20px;
+  color: white;
+  font-size: 12px;
+  border-radius: 8px;
+  background-color: black;
+  cursor: pointer;
+`;
+
+const JoinArea = styled.div`
+  margin-top: 20px;
+  text-align: center;
+`;
+
+const Text = styled.span`
+  color: #929292;
+  font-size: 12px;
+`;
+
+const GoToSignUp = styled.a`
+  margin-left: 4px;
+  color: #3c72ff;
+  font-size: 12px;
+  cursor: pointer;
+`;
